33eb823108c2de826c09c43d0c809955e6498009
ZweigDen <65008094+ZweigDen@users.noreply.github.com>
2020-07-27 17:21:17 +0800

    Commit to master

--- main/assets/js/all.js
+++ main/assets/js/all.js
@@ -1,16 +1,12 @@
 var uuid = 'e36c04b7-caa5-4a11-ab85-cd3ab214a719';
-var token = 'Cw81dB0hyYgmiXU480aaUWxrzLUxDE07A2JTiKNzcFInK56KemJiRPyGXyXY';
 var apiPath = 'https://course-ec-api.hexschool.io/';
-var api = `${apiPath}api/${uuid}/admin/ec/products`;
-// console.log(api);
-axios.defaults.headers['Authorization'] = `Bearer ${token}`;
+var api = `${apiPath}api/${uuid}/ec/products`;
+var products = [];
 
-axios.get(api)
-  .then(function(response){
+axios.get(api).then(function(response){
   products = response.data.data;
-  console.log(products);
   render();
-})
+});
 
 function render(){
   var app = document.getElementById('app');
@@ -26,10 +22,9 @@ function render(){
           <button class="btn btn-purple btn-block">價格：${item.price} 元</>
         </div>
       </div>
-    </div>`
+    </div>`;
     str+=content;
-    console.log(item.title);
-  })
+  });
   app.innerHTML = str;
 }
 
